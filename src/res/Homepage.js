2589dea178f3300f6c300d25650f7d355c172756
Durid <62970031+Ytonnah@users.noreply.github.com>
2024-06-11 16:45:10 +0800

    initials

---
+++ src/res/Homepage.js
@@ -0,0 +1,17 @@
+
+export default function Homepage(){
+    return(
+        <div>
+            <h1>Welcome to NCF Voting Sytem</h1>
+            
+        </div>
+    );
+}
+
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+}
